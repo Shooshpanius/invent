65e65470dedf732f2682a0cdac49ff3aefe3430a
Shooshpanius <admin@in-da-house.ru>
2014-05-31 09:45:45 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invent::Application.config.secret_key_base = '13d0c34c48bc8b78254db66d12277feb8169b722992cbf1294c873717217a9b3d96b1c74ce32ac7c7a14fc7524cdd8708d7a8aa0c2ec2cb6c4092178ca41ad7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Invent::Application.config.session_store :cookie_store, key: '_invent_session'
