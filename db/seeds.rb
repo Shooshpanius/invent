62cf76b483fa0d5c47bec2fd3d7f44e3423240ef
Shooshpanius <admin@in-da-house.ru>
2014-06-02 20:35:12 +0400

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(
+    login: 'admin',
+    username: 'admin',
+    pwd: '12345',
+    adm: 1,
+    active: 1
+
+)
