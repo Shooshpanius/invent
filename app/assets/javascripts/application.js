dcfeca989a777d3f61f24b55bac8ed5994f21a12
Shooshpanius <admin@in-da-house.ru>
2014-06-03 21:25:48 +0400

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,11 @@
 //= require additional-methods
 //= require bootstrap
 //= require jquery.maskedinput
-//= require jquery-tablesorter
+//= require jquery-tablesorter/jquery.metadata
+//= require jquery-tablesorter/jquery.tablesorter
+//= require jquery-tablesorter/jquery.tablesorter.widgets
 //= require jquery-tablesorter/addons/pager/jquery.tablesorter.pager
+//= require jquery-tablesorter/widgets/widget-repeatheaders
+//= require jquery-tablesorter/parsers/parser-metric
+//= require jquery-tablesorter/extras/jquery.quicksearch
 //= require_self
