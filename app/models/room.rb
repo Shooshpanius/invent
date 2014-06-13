3dddf394519e4859dc48baf2b5853e24b4a3a67d
Shooshpanius <admin@in-da-house.ru>
2014-06-13 19:49:40 +0400

    Test

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ActiveRecord::Base
 
-  has_many :workplaces, dependent: :restrict_with_exception
+  has_many :workplaces, dependent: :restrict_with_error
   belongs_to :territory
 
 end
