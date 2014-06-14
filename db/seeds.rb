cfd1292a2449bb20a81b927854d01cee7fa3a11b
Shooshpanius <admin@in-da-house.ru>
2014-06-14 09:20:48 +0400

    Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 User.create(
     login: 'admin',
     username: 'admin',
     pwd: '12345',
     adm: 1,
     active: 1
+)
+
+territory = Territory.create(
+    name: '---',
+    address: '---'
+)
+
+room = territory.rooms.create(
+
 
 )
